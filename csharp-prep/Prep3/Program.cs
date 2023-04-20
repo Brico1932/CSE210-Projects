b8a5833a14bd78ea0fcaf70a8a245c8e45a1390f
Brandon Rico <ric18040@byui.edu>
2023-04-20 17:08:02 -0600

    I completed the assignment for prep activity number 3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magic_number = randomGenerator.Next(1, 100);
+        string guess_str = "n/a";
+        int guess = 9999;
+        int counter = 0;
+
+        
+        while (magic_number != guess)
+        {
+            Console.WriteLine("What is your guess?");
+            guess_str = Console.ReadLine();
+            guess = int.Parse(guess_str);
+            counter = counter + 1;
+
+            if (magic_number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magic_number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
+        Console.WriteLine("Congradulations! You guessed it!");
+        Console.WriteLine($"You made {counter} guesses.");
+
+
     }
 }

a63c26bd8712052f80a838c888d19c0591050bb0
Brandon Rico <ric18040@byui.edu>
2023-04-20 19:23:30 -0600

    I completed the assignment for prep number 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,28 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List <int> numbers = new List<int>();
+        string new_number_str = "n/a";
+        int new_number = 9999;
+
+        while (new_number != 0)
+        {
+            Console.WriteLine("Enter number:");
+            new_number_str = Console.ReadLine();
+            new_number = int.Parse(new_number_str);
+            if (new_number != 0)
+            {
+                numbers.Add(new_number);
+            }
+        }
+        Console.WriteLine($"The sum is: {numbers.Sum()}");
+        Console.WriteLine($"The average is: {numbers.Average()}");
+        Console.WriteLine($"The largest number is: {numbers.Max()}");
+    
     }
 }

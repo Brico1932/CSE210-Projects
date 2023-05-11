3fc2672457c4c9a9f89e0f7d5782b339a3db40d2
Brandon Rico <ric18040@byui.edu>
2023-05-10 18:07:48 -0600

    I finished the assignment for week 4 prepare.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Geovani Rocket", "Pokemon studies");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Jessie Meowth","Muliplication","5.4","2-10");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("James Meowth","U.S. History","Manifest destiny");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInfo());
     }
 }

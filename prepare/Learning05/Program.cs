cc0b409af3dd25a80603f8c70eea5675bbb9c87d
Brandon Rico <ric18040@byui.edu>
2023-05-17 15:42:25 -0600

    I completed the assignment for W05 prepare.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square S1 = new Square("Orange", 5);
+        shapes.Add(S1);
+        Circle C2 = new Circle("Blue", 4);
+        shapes.Add(C2);
+        Rectangle R3 = new Rectangle("Yellow", 3, 5);
+        shapes.Add(R3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
